c733c77c4b9893890ac4e2079e452fa791718029
rjlopezdev <personal@rjlopezmad.com>
2019-01-18 00:12:48 +0100

    feat(api): export initial public api

--- projects/ngx-guardian/src/public_api.ts
+++ projects/ngx-guardian/src/public_api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ngx-guardian
  */
 
-export * from './lib/ngx-guardian.service';
-export * from './lib/ngx-guardian.component';
-export * from './lib/ngx-guardian.module';
+export { NgxGuardianService } from './lib/ngx-guardian.service';
+export { NgxGuardianComponent } from './lib/ngx-guardian.component';
+export { NgxGuardianModule } from './lib/ngx-guardian.module';
+export { NgxGuardianConfig } from './lib/config';
+export * from './lib/ngx-guardian-interfaces';
