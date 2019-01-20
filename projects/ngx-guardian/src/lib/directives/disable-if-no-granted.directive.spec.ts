2cbe65c1b5ec0bba2c751317d951e1371185b44e
rjlopezdev <personal@rjlopezmad.com>
2019-01-20 23:04:29 +0100

    test(directive): add initial test to disableIfNoGranted directive

--- projects/ngx-guardian/src/lib/directives/disable-if-no-granted.directive.spec.ts
+++ projects/ngx-guardian/src/lib/directives/disable-if-no-granted.directive.spec.ts
@@ -1,8 +1,23 @@
 import { DisableIfNoGrantedDirective } from './disable-if-no-granted.directive';
+import { TestBed } from '@angular/core/testing';
+import { EditPizzaComponent } from '../../mocks/edit-pizza.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgxGuardianService } from '../ngx-guardian.service';
 
-describe('DisableIfNoGrantedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisableIfNoGrantedDirective();
-    expect(directive).toBeTruthy();
+xdescribe('DisableIfNoGrantedDirective', () => {
+  let fixture;
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [ EditPizzaComponent, DisableIfNoGrantedDirective],
+      providers: [NgxGuardianService],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .createComponent(EditPizzaComponent);
+    fixture.detectChanges();
+  });
+
+  it('should disable input', () => {
+    const input: HTMLElement = fixture.nativeElement.querySelector('blockToDisable');
+    expect(input.getAttribute('disabled')).toBeTruthy();
   });
 });
--- projects/ngx-guardian/src/mocks/edit-pizza.component.ts
+++ projects/ngx-guardian/src/mocks/edit-pizza.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   template: `
   <h2> Pizza Edit form </h2>
   <input class="blockToManage" *ngxShowIfGranted="EDIT - PIZZA" type="text" value="Carbonara">
+  <button class="blockToDisabled" *ngxDisableIfNoGranted="BURN - PIZZA"> Submit </button>
   `
 })
 export class EditPizzaComponent { }
