c2a4ac1d015941ab744b6264e7485c672d5b6085
rjlopezdev <personal@rjlopezmad.com>
2019-01-27 03:33:27 +0100

    test(model): add test for Role constuctor & getName

---
+++ projects/ngx-guardian/src/test/role.spec.ts
@@ -0,0 +1,14 @@
+import { Role } from '../lib/model/role';
+
+describe('PermissionManager class', () => {
+
+    it('#construstor should instantiate a role with @name equals to "DEFAULT" ', () => {
+        const role: any = new Role('DEFAULT');
+        expect(role.name).toBe('DEFAULT');
+    });
+
+    it('#construstor should instantiate a role with @name equals to "DEFAULT" ', () => {
+        const role = new Role('DEFAULT');
+        expect(role.getName()).toBe('DEFAULT');
+    });
+});
