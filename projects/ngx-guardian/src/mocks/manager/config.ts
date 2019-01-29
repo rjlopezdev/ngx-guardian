776e3351e5805e172f0c8de9f29b31de04aa4be3
rjlopezdev <personal@rjlopezmad.com>
2019-01-29 14:46:57 +0100

    feat(mock): add resources

--- projects/ngx-guardian/src/mocks/manager/config.ts
+++ projects/ngx-guardian/src/mocks/manager/config.ts
@@ -6,17 +6,5 @@ export const ngxGuardianConfig: NgxGuardianConfig = {
     managers: [
         defaultManager
     ],
-    defaultRole: NgxGuardianRole.DEFAULT,
-    /**
-     * Remove this comment to set a default role from localStorage variable 'ngx-guardian-role'
-     */
-    // setFromStorage: true,
-    /**
-     * Remove this comment to set route to navigate if user is no auth (default: '/no-auth')
-     */
-    // unauthorizedRoute: '',
-    /**
-     * Remove this comment to set route to navigate if user has no permissions (default: '/no-granted')
-     */
-    // noGrantedRoute: ''
+    defaultRole: NgxGuardianRole.DEFAULT
 };
--- projects/ngx-guardian/src/mocks/manager/resources.ts
+++ projects/ngx-guardian/src/mocks/manager/resources.ts
@@ -4,3 +4,17 @@ export const FOO: NgxGuardianResource = {
     name: 'FOO',
     routes: []
 };
+
+export const PIZZA: NgxGuardianResource = {
+    name: 'PIZZA',
+    routes: [
+        '/pizza'
+    ]
+};
+
+export const TACO: NgxGuardianResource = {
+    name: 'TACO',
+    routes: [
+        '/taco'
+    ]
+};
