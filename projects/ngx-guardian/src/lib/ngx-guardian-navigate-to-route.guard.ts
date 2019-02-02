ceef8d87bf30793193b9b6b76ec302533672f1c0
rjlopezdev <personal@rjlopezmad.com>
2019-02-02 13:30:58 +0100

    feat(service): guard implementation

--- projects/ngx-guardian/src/lib/ngx-guardian-navigate-to-route.guard.ts
+++ projects/ngx-guardian/src/lib/ngx-guardian-navigate-to-route.guard.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { NgxGuardianService } from './ngx-guardian.service';
+import { Statement } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NgxGuardianNavigateToRouteGuard implements CanActivate {
 
-  constructor(private permissionManager: NgxGuardianService) {}
+  constructor(
+    private permissionManager: NgxGuardianService,
+    private router: Router
+  ) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.permissionManager.canNavigateTo(next.url.toString());
+    if (this.isGranted(state.url)) {
+      return true;
+    } else {
+      this.router.navigate(['/no-auth']);
+      return false;
+    }
+  }
+
+  isGranted(url): boolean {
+    return this.permissionManager.canNavigateTo(url)
+    || this.permissionManager.noGrantedRoute === url
+    || this.permissionManager.unauthorizedRoute === url;
   }
 }
